120e83251789c9e21363747bc2d14fb2ab8d1723
BTAnxoCSP <a25anxobt@iessanclemente.net>
2025-11-23 21:13:45 +0100

    Add files via upload

---
+++ Area_Triangulo.py
@@ -0,0 +1,9 @@
+
+print("--- Cálculo de Área do Triángulo ---")
+
+base = float(input("Introduce a base (cm): "))
+altura = float(input("Introduce a altura (cm): "))
+
+area = (base * altura) / 2
+
+print(f"\nO área do triángulo é de {area:.2f} cm².")
